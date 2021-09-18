c1c1d9346b1e3f9bf65d7fc02267f26dc29fdc00
Suvrat Mishra <suvrat29@gmail.com>
2021-09-19 01:55:52 +0530

    Hangfire Support
    Added Hangfire support to enable background scheduling of some logging activities.

---
+++ ViewModels/TableVM/UseractivitylogVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace track_expense.api.ViewModels.TableVM
+{
+    [Table("useractivitylog")]
+    public class UseractivitylogVM
+    {
+        [Key]
+        public long id { get; set; }
+        public int activitytype { get; set; }
+        public int actiontype { get; set; }
+        public long userid { get; set; }
+        public DateTime actiondate { get; set; }
+        public string actionfield1 { get; set; }
+        public string actionfield2 { get; set; }
+    }
+}
